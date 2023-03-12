bf250f0cbd49b728a0ba150bd92536a0b0754e19
Tom Buckman <bomtucker343@gmail.com>
2023-03-12 11:06:35 -0600

    Add main viewport and subcomponents. Still no logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountsComponent } from './accounts/accounts.component';
+import { MainViewPortComponent } from './main-view-port/main-view-port.component';
+import { PerformanceComponent } from './performance/performance.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountsComponent,
+    MainViewPortComponent,
+    PerformanceComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/main-view-port/main-view-port.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-view-port',
+  templateUrl: './main-view-port.component.html',
+  styleUrls: ['./main-view-port.component.scss']
+})
+export class MainViewPortComponent {
+
+}
